f997304f71bdfb4fe6e6417551a50f6df3d8f09e
SanyuZhang <deviltrigger1120@gmail.com>
2018-12-16 15:34:00 -0500

    Updated text generator with smarter response in the replies.

--- backend/src/text_generator.py
+++ backend/src/text_generator.py
@@ -2,6 +2,7 @@ import random
 from utter_more import UtterMore
 
 
+TOPIC_UNKNOWN = -1
 TOPIC_UNKNOWN = 0
 TOPIC_EXP = 1
 TOPIC_TIME = 2
@@ -61,6 +62,11 @@ def generate_topic(topic_id):
             "On a scale one to four, with four being the highest, what's your activity level?",
             "Do you excercise very often?",
             "How often do you excercise?"
+        )
+    elif topic_id = TOPIC_WELCOME:
+        um = UtterMore(
+            "(Greetings, this is|Welcome to) Dog Matcher. I (will|can) help you find a dream (dog|puppy). Let's (start|begin) with a question.",
+            "(Hi|Hello), I'm Dog Matcher. (Let me|I will try my best to) find a dream (dog|puppy) for you. First, let's (start|begin) with a question.",
         )
     else:
         um = UtterMore(
