7968a7bca07dc58e383ab73233276d36abf9b731
SanyuZhang <deviltrigger1120@gmail.com>
2018-12-15 22:54:08 -0500

    More options provided for utterance generation.

--- text_generator.py
+++ text_generator.py
@@ -21,36 +21,41 @@ def generate_topic(topic_id):
         )
     elif topic_id == TOPIC_TIME_ID:
         um = UtterMore(
-            "How many hours (per|every) week (can|could) you (put into|spend on) (trainning|playing with) your dog?"
+            "How many hours (per|every) week (can|could) you (put into|spend on) (trainning|playing with) your dog?",
         )
     elif topic_id == TOPIC_HOME_ID:
         um = UtterMore(
             "Do you live in an apartment or a house?",
-            "What kind of home do you live in, a house or an apartment?"
+            "What kind of home do you live in, a house or an apartment?",
         )
     elif topic_id == TOPIC_APT_ID:
         um = UtterMore(
-            "sample..."
+            "Do you just want to see dogs that are suitable for an apartment?",
+            "Would you like to see apartment dogs only?",
         )
     elif topic_id == TOPIC_NOISE_ID:
         um = UtterMore(
-            "sample..."
+            "Would you like your dog to be quieter or more vocal?",
+            "On a scale one to five, with five being the highest, what's your tolerence level for barking?",
         )
     elif topic_id == TOPIC_SHED_ID:
         um = UtterMore(
-            "sample..."
+            "On a scale one to five, with five being the highest, what's your tolerence level for shedding?",
+            "What's your expectation of shedding, frequent or infrequent?",
         )
     elif topic_id == TOPIC_KIDS_ID:
         um = UtterMore(
-            "sample..."
+            "Do you have kids under ten?",
         )
     elif topic_id == TOPIC_ACT_ID:
         um = UtterMore(
-            "sample..."
+            "On a scale one to five, with five being the highest, what's your activity level?",
         )
     else:
         um = UtterMore(
-            "sample..."
+            "Sorry, I cannot understand.",
+            "Sorry, I did not catch that.",
+            "Sorry, can you speak it again?"
         )
     um.iter_build_utterances()
     return random.choice(um.utterances)
