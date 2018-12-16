3d647cacb46e3310cf01d4e881f2925c6b4e8989
SanyuZhang <deviltrigger1120@gmail.com>
2018-12-16 18:42:05 -0500

    Update result_generator.py

--- backend/src/result_generator.py
+++ backend/src/result_generator.py
@@ -1,9 +1,9 @@
 # call elaborate_result(dog)
-
 import random
 import sqlite3
 from utter_more import UtterMore
 from config import *
+
 
 activity_level = ["","needs a lot of excercise","needs regular excercise","is energetic","is calm"]
 barking_level = ["","barks when necessary","doesn't like barking very much","tends to bark sometimes","barks frequently","likes to be vocal"]
@@ -12,9 +12,11 @@ trainability = ["", "maybe stubborn", "is agreeable", "is eager to please", "is 
 character = ["", "smallest dog breeds", "medium dog breeds", "largest dog breeds", "smartest dogs", "hypoallergenic dogs", "best family dogs", "best guard dogs", "best dogs for kids", "best dogs for apartments dwellers", "hairless dog breeds"]
 coat_type = ["","hairless","short","medium","long","smooth","wire"]
 
+
 def randomUtter(utters):
     utters.iter_build_utterances()    
     return random.choice(random.choice(utters.utterances))
+
 
 def elaborate_result(dog):
     '''
@@ -23,36 +25,82 @@ def elaborate_result(dog):
         output - String, desciption of the dog
     '''
     # get characteristic_id for the dog
+    dog_id = dog[0]
+    dog_name = dog[1]
+    dog_desc = dog[2]
+    dog_height_min = dog[3]
+    dog_height_max = dog[4]
+    dog_weight_min = dog[5]
+    dog_weight_max = dog[6]
+    dog_activity = dog[11]
+    dog_coat = dog[13]
+    dog_shed = dog[14]
+    dog_train = dog[16]
+    dog_popularity = dog[17]
+
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor() 
-    cursor.execute("SELECT characteristic_id FROM dogs_characteristics WHERE dog_id \
-                    ==" + str(dog[0]))
+    cursor.execute("SELECT characteristic_id FROM dogs_characteristics WHERE dog_id == %s" % (dog_id))
     charactId = cursor.fetchone()
+
     # building utterance 
     conf = UtterMore(
         "(Abosolutely|Sure)!",
         "No problem!",
-        "(I like this one too. |)Here you go!",
-        "Here's detailed info about " + dog[1] + ".",
+        "I like this one too. Here you go!",
+        "Here's detailed info about %s." % (dog_name),
         "That's (an excellent|a great) choice."
     )
+
+    content = dog_desc
+
+    if dog_height_min and dog_height_max:
+        content += " %s stands between %s and %s inches." % (dog_name, dog_height_min, dog_height_max)
+    elif dog_height_min:
+        content += " %s stands at least %s inches." % (dog_name, dog_height_min)
+    elif dog_height_max:
+        content += " %s stands at most %s inches." % (dog_name, dog_height_max)
+    if dog_weight_min and dog_weight_max:
+        content += " And he weights between %s and %s pounds." % (dog_weight_min, dog_weight_max)
+    elif dog_weight_min:
+        content += " And he weights minimum %s pounds." % (dog_weight_min)
+    elif dog_weight_max:
+        content += " And he weights maximum %s pounds." % (dog_weight_max)
+    if dog_popularity:
+        content += " %s %s amongst the most popular dogs breeds." % (dog_name, dog_popularity)
+    if charactId:
+        dog_charac = character[charactId[0]]
+    else:
+        dog_charac = character[0]
+    if dog_charac and dog_coat:
+        content += " He is one of the %s with a %s haircoat." % (dog_charac, dog_coat)
+    elif dog_charac:
+        content += " He is one of the %s." % (dog_charac)
+    elif dog_coat:
+        content += " He has %s haircoat." % (dog_coat)
+    if dog_shed:
+        content += " And he sheds %s." % (dog_shed)
+    if dog_activity and dog_train:
+        content += " %s %s , and %s when trainning." % (dog_name, dog_activity, dog_train)
+    elif dog_activity:
+        content += " %s %s." % (dog_name, dog_activity)
+    elif dog_train:
+        content += " %s %s when trainning." % (dog_name, dog_train)
+
     des = UtterMore(
-        dog[2] + "He stands between " + str(dog[3]) + " and " + str(dog[4]) + " inches, and weights maximum " +\
-        str(dog[6]) + " pounds. The " + dog[1] + " " + dog[17] + " amoung the most popular dogs breeds. " +\
-        "He is one of the " + character[charactId[0]] + " with a " + coat_type[dog[13]] + " haircoat and he sheds " + shed_level[dog[14]] +\
-        ". " + dog[1] + " " + activity_level[dog[11]] + ", and " + trainability[dog[16]] + " when trainning."
+        content
     )
     com = UtterMore(
-        "I'm sure he'll be a strong competitor for the best family member award in your house!"
-        "I'm sure you'll like it!"
-        "That's right, no animal can equal the " + dog[1]+ " as a pet."
+        " I'm sure he'll be a strong competitor for the best family member award in your house!",
+        " I'm sure you'll like it!",
+        " That's right, no animal can equal the %s as a pet." % (dog_name),
     )
     return randomUtter(conf) + " " + randomUtter(des) + " " + randomUtter(com)
+
 
 if __name__ == '__main__':
     connection = sqlite3.connect("dogs.db3") 
     cursor = connection.cursor() 
-    cursor.execute("SELECT * FROM dogs WHERE id == 5")
-    dog = cursor.fetchone()
-    #print(dog)
+    cursor.execute("SELECT * FROM dogs")
+    dog = cursor.fetchall()
     print(elaborate_result(dog))
